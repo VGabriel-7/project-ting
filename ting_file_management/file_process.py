f7a71e23632199aa7c39ef1b53bc252cbe808c57
Vinícius Gabriel <viniciusgsa99@gmail.com>
2023-04-04 18:42:13 -0300

    Requisito 3 conluído. Implementando função process.

--- ting_file_management/file_process.py
+++ ting_file_management/file_process.py
@@ -1,5 +1,21 @@
+from .file_management import txt_importer
+
+
 def process(path_file, instance):
-    """Aqui irá sua implementação"""
+    importer = txt_importer(path_file)
+    process = {
+        "nome_do_arquivo": path_file,
+        "qtd_linhas": len(importer),
+        "linhas_do_arquivo": importer
+    }
+
+    for i in range(0, len(instance)):
+        if instance.search(i)["nome_do_arquivo"] == process["nome_do_arquivo"]:
+            return
+    
+    instance.enqueue(process)
+    print(f'{process}')
+
 
 
 def remove(instance):
