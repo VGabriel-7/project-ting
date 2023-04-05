4a4d4804d98292f2f0260fb7374a985e7d6ad4b2
Vinícius Gabriel <viniciusgsa99@gmail.com>
2023-04-05 15:11:47 -0300

    Requisito 8 conluído. Implementando função search_by_word, get_word_occurrences_in_files e find_word_in_lines que fazer parte de uma quebra de funcionalidades para atender a um único propósito de encontrar palavras e mostrar a ocorrências delas e em que linhas elas estão nos arquivos.

--- ting_word_searches/word_search.py
+++ ting_word_searches/word_search.py
@@ -1,4 +1,19 @@
-def exists_word(word, instance):
+def find_word_in_lines(word, file):
+    ocorrencias = []
+
+    for idx, line in enumerate(file["linhas_do_arquivo"]):
+        if word.lower() in line.lower():
+            ocorrencias.append(
+                {
+                    "linha": idx + 1,
+                    "conteudo": line
+                }
+            )
+
+    return ocorrencias
+
+
+def get_word_occurrences_in_files(word, instance, type):
     search = {
         "palavra": word,
         "arquivo": "",
@@ -11,10 +26,13 @@ def exists_word(word, instance):
     for index in range(0, len(instance)):
         file = instance.search(index)
         search["arquivo"] = file["nome_do_arquivo"]
-
-        for idx, line in enumerate(file["linhas_do_arquivo"]):
-            if word.lower() in line.lower():
-                search["ocorrencias"].append({"linha": idx + 1})
+        if type == 'exists':
+            search["ocorrencias"] = [
+                {"linha": line["linha"]}
+                for line in find_word_in_lines(word, file)
+            ]
+        elif type == 'search':
+            search["ocorrencias"] = find_word_in_lines(word, file)
 
         if len(search["ocorrencias"]) > 0:
             all_word_searched.append(search)
@@ -22,5 +40,13 @@ def exists_word(word, instance):
     return all_word_searched
 
 
+def exists_word(word, instance):
+    return get_word_occurrences_in_files(
+        word=word, instance=instance, type='exists'
+    )
+
+
 def search_by_word(word, instance):
-    """Aqui irá sua implementação."""
+    return get_word_occurrences_in_files(
+        word=word, instance=instance, type='search'
+    )
